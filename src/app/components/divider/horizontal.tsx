0054da2eab6bce3a2bfa3ac5b7aefb517fda3a75
Vicente <pvicenteneto7@gmail.com>
2023-09-22 09:03:20 -0300

    feat: criação de divisor de sessões

---
+++ src/app/components/divider/horizontal.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+
+import { cn } from "@/app/lib/utis"
+
+type HorizontalDividerProps = {
+    className?: string
+}
+
+export const HorizontalDivider = ({className}: HorizontalDividerProps) => {
+    return(
+        <div className={cn('w-full my-8 border-b border-b-gray-800', className)} />
+    )
+}
